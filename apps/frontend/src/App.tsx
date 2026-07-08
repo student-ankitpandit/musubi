637a027baf82347b73b7ef203c641e2085602284
student-ankitpandit <ankitpanditwork120@gmail.com>
2026-07-08 14:00:05 +0530

    init project

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { APITester } from "./APITester";
+import "./index.css";
+
+import logo from "./logo.svg";
+import reactLogo from "./react.svg";
+
+export function App() {
+  return (
+    <h1 className="text-9xl">hi</h1>
+  );
+}
+
+export default App;
